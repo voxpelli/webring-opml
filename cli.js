fd4db48d32ede1f6f64e15fadf00c42f63a3551d
Pelle Wessman <pelle@kodfabrik.se>
2023-06-23 23:16:04 +0200

    Make the CLI create files and folder

--- cli.js
+++ cli.js
@@ -1,15 +1,57 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+import { mkdir, writeFile } from 'node:fs/promises';
+import path from 'node:path';
 import { argv } from 'node:process';
 
 import { generateOpmlForWebring } from './index.js';
 
-const webringName = argv[2];
+const outDir = argv[2];
+const webringNames = argv.slice(3);
 
-if (!webringName) {
-  console.error('Error: A webri.ng name is requried');
+console.log(webringNames, outDir);
+if (!outDir) {
+  console.error('Error: An out diris required');
   process.exit(1);
 }
 
-console.log(await generateOpmlForWebring(webringName));
+if (webringNames.length === 0) {
+  console.error('Error: At least one webri.ng name required');
+  process.exit(1);
+}
+
+try {
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  await mkdir(outDir);
+} catch {
+  // Its okay if it already exists
+}
+
+const settled = await Promise.allSettled(webringNames.map(async (name) => {
+  const opml = await generateOpmlForWebring(name);
+
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  await writeFile(
+    path.join(outDir, name + '.opml'),
+    opml,
+    'utf8'
+  );
+
+  return name;
+}));
+
+let failed = false;
+
+for (const status of settled) {
+  if (status.status === 'rejected') {
+    console.error('Failed generating a webri.ng OPML because:', status.reason);
+    failed = true;
+  } else {
+    console.log('Successfully created webri.ng OPML for:', status.value);
+  }
+}
+
+if (failed) {
+  process.exit(1);
+}
